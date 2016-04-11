413d9bf09199d2d7126c3aa69abac6729510693f
matthewyao <mosmovon@gmail.com>
2016-04-11 22:08:35 +0800

    version 2

--- src/com/matthewyao/TankClient.java
+++ src/com/matthewyao/TankClient.java
@@ -1,6 +1,8 @@
 package com.matthewyao;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by matthewyao on 2016/4/11.
@@ -9,7 +11,15 @@ public class TankClient extends Frame{
 
     public void lauchFrame(){
         this.setLocation(400,300);
-        this.setSize(800,600);
+        this.setSize(800, 600);
+        this.setTitle("TankWar");
+        this.setResizable(false);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
         this.setVisible(true);
     }
 
