2ac3a76f34434b077fac99ba1278e2dbf586c411
matthewyao <mosmovon@gmail.com>
2016-04-11 22:52:39 +0800

    version 3

--- src/com/matthewyao/TankClient.java
+++ src/com/matthewyao/TankClient.java
@@ -9,8 +9,10 @@ import java.awt.event.WindowEvent;
  */
 public class TankClient extends Frame{
 
+    private int x = 50, y = 50;
+
     public void lauchFrame(){
-        this.setLocation(400,300);
+        this.setLocation(400, 300);
         this.setSize(800, 600);
         this.setTitle("TankWar");
         this.setResizable(false);
@@ -20,11 +22,37 @@ public class TankClient extends Frame{
                 System.exit(0);
             }
         });
+        this.setBackground(Color.GREEN);
         this.setVisible(true);
+        new TankThread().run();
     }
 
     public static void main(String[] args) {
         TankClient client = new TankClient();
         client.lauchFrame();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x, y, 30, 30);
+        g.setColor(c);
+        x += 4;
+        y += 3;
+    }
+
+    private class TankThread implements Runnable{
+        @Override
+        public void run() {
+            while (true){
+                repaint();
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
