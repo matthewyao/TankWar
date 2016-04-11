59f1db04284164569b515fc4a2812cd3149be669
matthewyao <mosmovon@gmail.com>
2016-04-11 23:38:10 +0800

    version 5

--- src/com/matthewyao/TankClient.java
+++ src/com/matthewyao/TankClient.java
@@ -1,6 +1,10 @@
 package com.matthewyao;
 
+import javafx.scene.input.KeyCode;
+
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -9,13 +13,18 @@ import java.awt.event.WindowEvent;
  */
 public class TankClient extends Frame{
 
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 600;
+    public static final int GAME_REFRESH_TIME = 30;
+
     private int x = 50, y = 50;
+    private int inc = 5;
 
     private Image offScreenImage = null;
 
     public void lauchFrame(){
         this.setLocation(400, 300);
-        this.setSize(800, 600);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
         this.setTitle("TankWar");
         this.setResizable(false);
         this.addWindowListener(new WindowAdapter() {
@@ -25,6 +34,7 @@ public class TankClient extends Frame{
             }
         });
         this.setBackground(Color.GREEN);
+        this.addKeyListener(new KeyMonitor());
         this.setVisible(true);
         new TankThread().run();
     }
@@ -40,19 +50,17 @@ public class TankClient extends Frame{
         g.setColor(Color.RED);
         g.fillOval(x, y, 30, 30);
         g.setColor(c);
-        x += 4;
-        y += 3;
     }
 
     @Override
     public void update(Graphics g) {
         if (offScreenImage == null){
-            offScreenImage = this.createImage(800,600);
+            offScreenImage = this.createImage(GAME_WIDTH,GAME_HEIGHT);
         }
         Graphics gImage = offScreenImage.getGraphics();
         Color c = gImage.getColor();
         gImage.setColor(Color.GREEN);
-        gImage.fillRect(0,0,800,600);
+        gImage.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
         gImage.setColor(c);
         paint(gImage);
         g.drawImage(offScreenImage,0,0,null);
@@ -64,10 +72,23 @@ public class TankClient extends Frame{
             while (true){
                 repaint();
                 try {
-                    Thread.sleep(20);
+                    Thread.sleep(GAME_REFRESH_TIME);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+            }
+        }
+    }
+
+    private class KeyMonitor extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key){
+                case KeyEvent.VK_UP:y -= inc;break;
+                case KeyEvent.VK_RIGHT:x += inc;break;
+                case KeyEvent.VK_DOWN:y += inc;break;
+                case KeyEvent.VK_LEFT:x -= inc;break;
             }
         }
     }
